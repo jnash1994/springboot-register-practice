d317c4edc40715230d968b504029149d137b22ab
jnash1994 <bigearblack@gmail.com>
2023-07-11 10:50:24 +0800

    驗證錯誤訊息

--- register/src/main/java/Nash/example/register/controller/UserController.java
+++ register/src/main/java/Nash/example/register/controller/UserController.java
@@ -3,10 +3,12 @@ package Nash.example.register.controller;
 
 import Nash.example.register.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 
 @Controller
@@ -15,12 +17,15 @@ public class UserController {
 
 
     @GetMapping("/register")
-    public String doDefault(){
+    public String doDefault(User user){
         return "register";
     }
 
     @PostMapping("/register")
-    public String register(User user){
+    public String register(@Valid User user , BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "register";
+        }
         Date now = new Date();
         user.setRegDate(now);
         return "success";
--- register/src/main/java/Nash/example/register/model/User.java
+++ register/src/main/java/Nash/example/register/model/User.java
@@ -2,21 +2,31 @@ package Nash.example.register.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @AllArgsConstructor
 @Data
 public class User {
     private Integer id;
+    @NotBlank(message="使用者名稱不能為空")
+    @Length(min=4, max=12, message="使用者名稱長度必須在4到12字元之間")
     private String username;
+    @NotBlank(message="密碼不能為空")
+    @Length(min=4, max=8, message="密碼長度必須在4到18字元之間")
     private String password;
+    private String confirmPassword;
     private Boolean sex;
+    @NotBlank(message="郵件地址不能為空")
+    @Email(message="郵件地址必須視有效的郵件地址")
     private String email;
     private String pwdQuestion;
     private String pwdAnswer;
     private Date regDate;
     private Date lastLoginDate;
     private String lastLoginIp;
-}
 
+}
